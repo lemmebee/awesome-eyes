81f66af1d8f6c1ee0a51759a080d75804d8113cf
ehabshaaban <ehabsfeki@gmail.com>
2022-04-15 13:08:14 +0200

    feat: update grafana release

--- grafana.tf
+++ grafana.tf
@@ -1,33 +1,37 @@
 resource "kubernetes_secret" "grafana" {
   metadata {
-    name      = "grafana"
+    name      = var.grafana_kubernetes_secret_name
     namespace = var.namespace
   }
 
   data = {
-    admin-user     = "admin"
-    admin-password = random_password.grafana.result
+    admin-user     = var.grafana_admin_user
+    admin-password = var.grafana_admin_password
   }
-}
 
-resource "random_password" "grafana" {
-  length = 24
+  depends_on = [
+    helm_release.prometheus
+  ]
 }
 
 resource "helm_release" "grafana" {
-  chart      = "grafana"
-  name       = "grafana"
-  repository = "https://grafana.github.io/helm-charts"
+  chart      = var.grafana_chart
+  name       = var.grafana_name
+  repository = var.grafana_repository
   namespace  = var.namespace
-  version    = "6.24.1"
+  version    = var.grafana_version
 
   values = [
     templatefile("${path.module}/templates/grafana-values.yaml", {
       admin_existing_secret = kubernetes_secret.grafana.metadata[0].name
-      admin_user_key        = "admin-user"
-      admin_password_key    = "admin-password"
+      admin_user_key        = var.grafana_admin_user_key
+      admin_password_key    = var.grafana_admin_password_key
       prometheus_svc        = "${helm_release.prometheus.name}-server"
-      replicas              = 1
+      replicas              = var.grafana_replicas
     })
+  ]
+
+  depends_on = [
+    kubernetes_secret.grafana
   ]
 }
