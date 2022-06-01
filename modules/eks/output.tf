51040407a9e5c6a43016bd1ab7523239d92bbcc7
ehabshaaban <ehabsfeki@gmail.com>
2022-06-01 13:00:53 +0200

    feat: Output node group name

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -8,3 +8,8 @@ output "region" {
   description = "aws region"
   value       = var.region
 }
+
+output "node_group_name" {
+  description = "Eks node group name"
+  value       = aws_eks_node_group.this.node_group_name
+}
